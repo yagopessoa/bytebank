2ab422d00d196e3b91107e9d1be9d444985842ec
Yago <yagorp15@gmail.com>
2020-08-11 06:27:26 -0300

    feat(layout): create home page base layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  // runApp(MyApp());
-  runApp(
-    Column(
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Transferências'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () => {},
+          ),
+          body: TransfersList(),
+        ),
+      ),
+    );
+
+class TransfersList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
       children: <Widget>[
-        Text(
-          '\n\nBem vindo ao ByteBank',
-          textDirection: TextDirection.ltr,
-        ),
-        Text(
-          '\n\nUm app de gerenciamento de transações',
-          textDirection: TextDirection.ltr,
-        ),
-        Column(
-          children: [
-            Text(
-              '\n\nEnjoy!',
-              textDirection: TextDirection.ltr,
-            ),
-          ],
-        )
+        TransferItem(Transfer(5000.0, 5884)),
+        TransferItem(Transfer(2340.0, 5884)),
       ],
-    ),
-  );
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer transfer;
+
+  TransferItem(this.transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(transfer.value.toString()),
+        subtitle: Text(transfer.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final int accountNumber;
+
+  Transfer(this.value, this.accountNumber);
 }
