f0883675cca9b06e186a4882021156a50bbdeb06
Yago <yagorp15@gmail.com>
2020-08-13 06:26:45 -0300

    feat(form): create new transfer form

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,112 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Transferências'),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () => {},
-          ),
-          body: TransfersList(),
-        ),
-      ),
-    );
+void main() => runApp(BytebankApp());
 
-class TransfersList extends StatelessWidget {
+class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        TransferItem(Transfer(5000.0, 5884)),
-        TransferItem(Transfer(2340.0, 5884)),
-      ],
+    return MaterialApp(
+      home: Scaffold(
+        body: NewTransferForm(),
+      ),
     );
   }
 }
 
-class TransferItem extends StatelessWidget {
+class NewTransferForm extends StatelessWidget {
+  final TextEditingController _accNumberField = TextEditingController();
+  final TextEditingController _valueField = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criando transferência'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _accNumberField,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                labelText: 'Número da conta',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _valueField,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Valor',
+                hintText: '0.00',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: RaisedButton(
+              child: Text('Confirmar'),
+              color: Colors.blue,
+              textColor: Colors.white,
+              onPressed: () {
+                final int accNumber = int.tryParse(_accNumberField.text);
+                final double value = double.tryParse(_valueField.text);
+
+                if (accNumber != null && value != null) {
+                  final transfer = Transfer(value, accNumber);
+
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                    content: Text(
+                      transfer.toString(),
+                    ),
+                  ));
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TransfersList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: <Widget>[
+          TransferListItem(Transfer(5000.0, 5884)),
+          TransferListItem(Transfer(2340.0, 5884)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => {},
+      ),
+    );
+  }
+}
+
+class TransferListItem extends StatelessWidget {
   final Transfer transfer;
 
-  TransferItem(this.transfer);
+  TransferListItem(this.transfer);
 
   @override
   Widget build(BuildContext context) {
@@ -49,4 +125,9 @@ class Transfer {
   final int accountNumber;
 
   Transfer(this.value, this.accountNumber);
+
+  @override
+  String toString() {
+    return 'Transferência: { Conta: $accountNumber, Valor: $value }';
+  }
 }
