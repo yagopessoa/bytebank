bf14e5769426def4b1c00a07bab835632db67bcb
Yago <yagorp15@gmail.com>
2020-08-14 08:05:46 -0300

    feat(transfers): listing created transfers

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,27 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: NewTransferForm(),
+      home: TransfersList(),
+      theme: ThemeData(
+        primaryColor: Colors.blueGrey[700],
+        accentColor: Colors.tealAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.tealAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
       ),
     );
   }
 }
 
-class NewTransferForm extends StatelessWidget {
+class NewTransferForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return NewTransferFormState();
+  }
+}
+
+class NewTransferFormState extends State<NewTransferForm> {
   final TextEditingController _accNumberField = TextEditingController();
   final TextEditingController _valueField = TextEditingController();
 
@@ -21,100 +34,144 @@ class NewTransferForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Criando transferência'),
+        title: Text('Criar transferência'),
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            CustomTextField(
               controller: _accNumberField,
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                labelText: 'Número da conta',
-                hintText: '0000',
-              ),
-              keyboardType: TextInputType.number,
+              label: 'Número da conta',
+              hint: '0000',
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
+            CustomTextField(
               controller: _valueField,
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                icon: Icon(Icons.monetization_on),
-                labelText: 'Valor',
-                hintText: '0.00',
-              ),
-              keyboardType: TextInputType.number,
+              label: 'Valor (R\$)',
+              hint: '0.00',
+              icon: Icons.monetization_on,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 16.0),
-            child: RaisedButton(
-              child: Text('Confirmar'),
-              color: Colors.blue,
-              textColor: Colors.white,
-              onPressed: () {
-                final int accNumber = int.tryParse(_accNumberField.text);
-                final double value = double.tryParse(_valueField.text);
-
-                if (accNumber != null && value != null) {
-                  final transfer = Transfer(value, accNumber);
-
-                  Scaffold.of(context).showSnackBar(SnackBar(
-                    content: Text(
-                      transfer.toString(),
-                    ),
-                  ));
-                }
-              },
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: RaisedButton(
+                child: Text('Confirmar'),
+                // color: Colors.blue,
+                // textColor: Colors.white,
+                onPressed: () => _createTransfer(context),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
+
+  void _createTransfer(BuildContext context) {
+    final int accNumber = int.tryParse(_accNumberField.text);
+    final double value = double.tryParse(_valueField.text);
+
+    if (accNumber != null && value != null) {
+      final newTransfer = Transfer(value, accNumber);
+      Navigator.pop(context, newTransfer);
+    }
+  }
 }
 
-class TransfersList extends StatelessWidget {
+class TransfersList extends StatefulWidget {
+  final List<Transfer> _transfers = List();
+
+  @override
+  State<StatefulWidget> createState() {
+    return TransferListState();
+  }
+}
+
+class TransferListState extends State<TransfersList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Transferências'),
       ),
-      body: Column(
-        children: <Widget>[
-          TransferListItem(Transfer(5000.0, 5884)),
-          TransferListItem(Transfer(2340.0, 5884)),
-        ],
+      body: ListView.builder(
+        itemCount: widget._transfers.length,
+        itemBuilder: (context, index) {
+          final transfer = widget._transfers[index];
+          return TransferListItem(transfer, key: UniqueKey());
+        },
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () => {},
+        onPressed: () {
+          final Future<Transfer> future = Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) {
+              return NewTransferForm();
+            }),
+          );
+
+          future.then((transfer) => _addTransfer(context, transfer));
+        },
+      ),
+    );
+  }
+
+  void _addTransfer(context, newTransfer) {
+    if (newTransfer != null) {
+      setState(() {
+        widget._transfers.add(newTransfer);
+      });
+
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text(
+          'Transferência adicionada com sucesso!',
+        ),
+      ));
+    }
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final IconData icon;
+  final String label;
+  final String hint;
+
+  const CustomTextField(
+      {Key key, this.controller, this.icon, this.label, this.hint})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controller,
+        style: TextStyle(
+          fontSize: 24.0,
+        ),
+        decoration: InputDecoration(
+          icon: icon != null ? Icon(icon) : null,
+          labelText: label,
+          hintText: hint,
+        ),
+        keyboardType: TextInputType.number,
       ),
     );
   }
 }
 
 class TransferListItem extends StatelessWidget {
-  final Transfer transfer;
+  final Transfer _transfer;
 
-  TransferListItem(this.transfer);
+  TransferListItem(this._transfer, {Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         leading: Icon(Icons.monetization_on),
-        title: Text(transfer.value.toString()),
-        subtitle: Text(transfer.accountNumber.toString()),
+        title: Text(_transfer.value.toString()),
+        subtitle: Text(_transfer.accountNumber.toString()),
       ),
     );
   }
